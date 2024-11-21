a7e1ce4cf47a0e7c81343361b0e44a2fd7f02cce
Haik <haik.asatryan.95@gmail.com>
2024-11-21 20:57:29 +0400

    upgraded to .net 9

--- src/EFCore.AuditBase/ModelBuilderExtensions.cs
+++ src/EFCore.AuditBase/ModelBuilderExtensions.cs
@@ -5,30 +5,31 @@ namespace EFCore.AuditBase;
 
 public static class ModelBuilderExtensions
 {
-    public static void FilterOutDeletedMarkedObjects(this ModelBuilder modelBuilder)
-    {
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            if (!typeof(AuditEntityBase).IsAssignableFrom(entityType.ClrType))
-            {
-                continue;
-            }
+   public static void FilterOutDeletedMarkedObjects(this ModelBuilder modelBuilder)
+   {
+      foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+      {
+         if (!typeof(AuditEntityBase).IsAssignableFrom(entityType.ClrType))
+         {
+            continue;
+         }
 
-            var parameter = Expression.Parameter(entityType.ClrType, "e");
-            var propertyInfo = entityType.ClrType.GetProperty(nameof(AuditEntityBase.Deleted));
+         var parameter = Expression.Parameter(entityType.ClrType, "e");
+         var propertyInfo = entityType.ClrType.GetProperty(nameof(AuditEntityBase.Deleted));
 
-            if (propertyInfo is null)
-            {
-                throw new InvalidOperationException(
-                    $"Expected property '{nameof(AuditEntityBase.Deleted)}' not found on '{entityType.ClrType.Name}'.");
-            }
+         if (propertyInfo is null)
+         {
+            throw new InvalidOperationException(
+               $"Expected property '{nameof(AuditEntityBase.Deleted)}' not found on '{entityType.ClrType.Name}'.");
+         }
 
-            var propertyAccess = Expression.MakeMemberAccess(parameter, propertyInfo);
+         var propertyAccess = Expression.MakeMemberAccess(parameter, propertyInfo);
 
-            var notExpression = Expression.Not(propertyAccess);
-            var lambda = Expression.Lambda(notExpression, parameter);
+         var notExpression = Expression.Not(propertyAccess);
+         var lambda = Expression.Lambda(notExpression, parameter);
 
-            modelBuilder.Entity(entityType.ClrType).HasQueryFilter(lambda);
-        }
-    }
+         modelBuilder.Entity(entityType.ClrType)
+                     .HasQueryFilter(lambda);
+      }
+   }
 }
